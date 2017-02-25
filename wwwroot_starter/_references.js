e3f7e4561e7d0cfadec5b44dd2fffb885e66f4d3
jasonwang1019 <jizzwang@jizzwang.localdomain>
2017-02-25 14:09:25 +0800

    move out

--- wwwroot_starter/_references.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="typings/node/node.d.ts" />
-/// <reference path="typings/express/express.d.ts" />
-/// <reference path="typings/jquery/jquery.d.ts" />
-/// <reference path="typings/nconf/nconf.d.ts" />
-/// <reference path="typings/socket.io/socket.io.d.ts" />
